e2f8bb959a43b3298d6f92b35273e09793b076c0
Jeremy Overman <joverman@collective.com>
2023-10-31 15:20:02 -0400

    updated readme and added selection support

--- main.ts
+++ main.ts
@@ -2,22 +2,39 @@ import slackifyMarkdown from 'slackify-markdown';
 import { Editor, MarkdownView, Plugin } from 'obsidian';
 
 export default class SlackifyNote extends Plugin {
-	private fixFormatting(md: string) {
-		let result = md.replace(/<(.*?)\|(.*?)>/g, '[$2]($1)')
+	private convertMdToSlack(md: string) {
+		const slackMarkdown = slackifyMarkdown(md)
+
+		// Replace Slack links with Markdown links. Obsidian already had this right, but slackify-markdown breaks it.
+		let result = slackMarkdown.replace(/<(.*?)\|(.*?)>/g, '[$2]($1)')
+
+		// Replace callouts with quotes
 		result = result.replace(/\[\!\w+\][-+]?\s+/, '')
 
 		return result;
 	}
 
 	async onload() {
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'slackify-note',
 			name: 'Copy note as Slack Markdown',
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				// Convert the note to Slack markdown
 				const noteMarkdown = editor.getValue()
-				const slackMarkdown = this.fixFormatting(slackifyMarkdown(noteMarkdown))
+				const slackMarkdown = this.convertMdToSlack(noteMarkdown)
+
+				// Copy the slackMarkdown to the clipboard
+				await navigator.clipboard.writeText(slackMarkdown)
+			}
+		});
+
+		this.addCommand({
+			id: 'slackify-selection',
+			name: 'Copy selection as Slack Markdown',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				// Convert the note to Slack markdown
+				const noteMarkdown = editor.getSelection()
+				const slackMarkdown = this.convertMdToSlack(noteMarkdown)
 
 				// Copy the slackMarkdown to the clipboard
 				await navigator.clipboard.writeText(slackMarkdown)
